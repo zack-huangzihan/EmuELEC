6899d2d090f3320c4090c3dbe1a86148eeba7c8e
ZiHan Huang <zack.huang@rock-chips.com>
2022-12-21 17:49:13 +0800

    update rga lib
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>

--- packages/lib32/graphics/lib32-librga/package.mk
+++ packages/lib32/graphics/lib32-librga/package.mk
@@ -6,18 +6,21 @@ PKG_NAME="lib32-librga"
 PKG_VERSION="$(get_pkg_version librga)"
 PKG_ARCH="aarch64"
 PKG_LICENSE="GNU"
-PKG_SITE="https://github.com/shantigilbert/linux-rga"
-PKG_URL=""
+#PKG_SITE="https://github.com/rockchip/linux-rga"
+PKG_URL="file:///home/huangzihan/third_party/game_project/EmuELEC/sources/librga/linux-rga.tar.gz"
+PKG_SOURCE_NAME="/home/huangzihan/third_party/game_project/EmuELEC/sources/librga/linux-rga"
 PKG_DEPENDS_TARGET="lib32-toolchain lib32-libdrm"
 PKG_DEPENDS_UNPACK+=" librga"
 PKG_PATCH_DIRS+=" $(get_pkg_directory librga)/patches"
 PKG_LONGDESC="The RGA driver userspace"
 PKG_TOOLCHAIN="auto"
 PKG_BUILD_FLAGS="lib32"
+GET_HANDLER_SUPPORT="git file"
 
 unpack() {
   mkdir -p ${PKG_BUILD}
-  tar --strip-components=1 -xf ${SOURCES}/librga/librga-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}
+  #tar --strip-components=1 -xf ${SOURCES}/librga/librga-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}
+  cp -r ${SOURCES}/librga/linux-rga/* ${PKG_BUILD}
 }
 
 post_makeinstall_target() {
--- projects/Rockchip/packages/libgo2/librga/package.mk
+++ projects/Rockchip/packages/libgo2/librga/package.mk
@@ -2,11 +2,14 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="librga"
-PKG_VERSION="1fc02d56d97041c86f01bc1284b7971c6098c5fb"
+#PKG_VERSION="1fc02d56d97041c86f01bc1284b7971c6098c5fb"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GNU"
 PKG_DEPENDS_TARGET="toolchain libdrm"
-PKG_SITE="https://github.com/shantigilbert/linux-rga"
-PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+#PKG_SITE="https://github.com/shantigilbert/linux-rga"
+PKG_URL="file:///home/huangzihan/third_party/game_project/EmuELEC/sources/librga/linux-rga.tar.gz"
+PKG_SOURCE_NAME="/home/huangzihan/third_party/game_project/EmuELEC/sources/librga/linux-rga"
+
 PKG_LONGDESC="The RGA driver userspace "
 PKG_TOOLCHAIN="auto"
+GET_HANDLER_SUPPORT="git file"
